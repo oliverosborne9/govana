15e587bcad5e49913ce96452a80881e25bdfc16f
oliverosborne9 <oliverosborne96@googlemail.com>
2023-05-06 19:22:54 +0100

    feat: make entire button change colour on hover including border
    feat: change incorrect answer card to brighter fuchsia colour

--- src/Card.tsx
+++ src/Card.tsx
@@ -9,24 +9,15 @@ type QuestionCardProps = {
 
 function QuestionCard(props: QuestionCardProps) {
 	const labelText = props.isCorrect ? '✓' : '?';
-	const bgCol = props.isCorrect ? 'bg-green-500' : 'bg-indigo-100';
-	const mainBorderCol = props.isCorrect
-		? 'border-green-500'
-		: 'border-indigo-100';
-	const hoverCol = props.isCorrect
-		? 'hover:bg-green-600'
-		: 'hover:bg-indigo-200';
-	const currQuestionFormat = props.isCurrQuestion
-		? 'ring-2 ring-offset-2 ring-indigo-500 font-extrabold'
-		: '';
+	const bgCol = props.isCorrect
+		? 'bg-green-500 border-green-500 hover:bg-green-600 hover:border-green-600'
+		: 'bg-fuchsia-500 border-fuchsia-500 hover:bg-fuchsia-600 hover:border-fuchsia-600';
 	const clueBorder = props.isClue ? ' border-b-cyan-500' : '';
-	const textCol = props.isCorrect
-		? 'text-white'
-		: 'text-black';
+	const currQuestionFormat = props.isCurrQuestion ? 'ring-2 ring-offset-2 ring-indigo-500 font-extrabold' : '';
 	return (
-		<button className={`${bgCol} font-mono border-4 ${mainBorderCol} h-auto items-center flex-col p-2  ${clueBorder} group relative w-full flex rounded justify-center
-                text-xs
-             	${hoverCol} ${currQuestionFormat} ${textCol}`}
+		<button className={`${bgCol} ${clueBorder} ${currQuestionFormat}
+							font-mono border-4  h-auto items-center flex-col p-2 group relative
+							w-full flex rounded justify-center text-xs text-white`}
 		onClick={props.onSubmitClick}
 		>
         Q{props.questionNum} {labelText}
